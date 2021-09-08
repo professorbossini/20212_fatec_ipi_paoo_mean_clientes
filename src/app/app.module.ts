502b400b8e4ee72d08d53fc5710b894319901817
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-08 16:03:24 -0300

    Criamos o cabecalho e encerramos a secao 2.6. Retomar da secao 2.7 da apostila 01 de mean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatToolbarModule } from '@angular/material/toolbar'
+
 
 import { AppComponent } from './app.component';
+import  { ClienteInserirComponent } from './clientes/cliente-inserir/cliente-inserir.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteInserirComponent,
+    CabecalhoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
